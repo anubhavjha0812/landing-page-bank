237b84784d5415469bef4598de64e63985618ec9
anubhavjha08 <anubhavjha0812@gmail.com>
2024-10-17 23:16:59 +0530

    8th commit

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -6,7 +6,7 @@ const NewsSection = () => {
   return (
     <section className="my-8 px-4 bg-gray-950 py-8">
       <h2 className="font-poppins text-3xl font-bold mb-4 text-white p-4" >
-        Latest News
+        STAY INSPIRED WITH OUT NEWS
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {newsData.slice(0, 3).map((news) => (
